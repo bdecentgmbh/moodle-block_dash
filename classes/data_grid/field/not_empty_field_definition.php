35ee42586b9ecdde65c9e918c8b9e98d392550e8
moodledev <moodledev@gmail.com>
2019-12-09 08:59:48 -0600

    Fix form warning, add strings

---
+++ classes/data_grid/field/not_empty_field_definition.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    block_dash
+ * @copyright  2019 bdecent gmbh <https://bdecent.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_dash\data_grid\field;
+
+/**
+ * Converts bool to yes or no label.
+ *
+ * @package block_dash\data_grid\field
+ */
+class not_empty_field_definition extends bool_field_definition
+{
+    /**
+     * After records are relieved from database each field has a chance to transform the data.
+     * Example: Convert unix timestamp into a human readable date format
+     *
+     * @param $data
+     * @param \stdClass $record Entire row
+     * @return mixed
+     * @throws \moodle_exception
+     */
+    public function transform_data($data, \stdClass $record)
+    {
+        return parent::transform_data(!empty($data), $record);
+    }
+}

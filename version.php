27890a52f50e41615caf1f918599f52709ab4703
moodledev <moodledev@gmail.com>
2020-09-23 08:30:08 -0500

    DASH-113 fix query debug rendering in totara

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020090101;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020092300;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017051509;        // Requires this Moodle version.
 $plugin->component = 'block_dash';      // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release = '1.0.2';
+$plugin->release = '1.1';

7f81ff43b7f247694df69f70a190fdc0ba28c386
Prasanna LMSACE <prasanna_t@lmsace.com>
2023-11-29 19:26:14 +0530

    Version upgraded

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023101706;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023101707;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700;        // Requires this Moodle version.
 $plugin->component = 'block_dash';      // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_RC;
 $plugin->release = '1.8 RC';
-$plugin->supported = [311, 401];
+$plugin->supported = [311, 403];

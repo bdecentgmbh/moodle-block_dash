b692213b4ef8889a5592fd75c0206e0bef329ac0
raja <raja.lmsace@gmail.com>
2025-03-27 18:14:14 +0530

    Include: Dash 2.5 activity completion widget strings. - DASH-1041

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025031900;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025032700;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700;        // Requires this Moodle version.
 $plugin->component = 'block_dash';      // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '2.4';
+$plugin->release = '2.5';
 $plugin->supported = [401, 405];

096c6aeebcc3a45b655e72f5db687b0427025958
Stefan-Alexander Scholz <hello@bdecent.de>
2025-07-02 08:45:29 +0200

    Update version.php

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->version   = 2025032900;        // The current plugin version (Date: YYY
 $plugin->requires  = 2021051700;        // Requires this Moodle version.
 $plugin->component = 'block_dash';      // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '2.6';
+$plugin->release = '2.5';
 $plugin->supported = [401, 405];

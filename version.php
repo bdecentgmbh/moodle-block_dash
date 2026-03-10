9cb5dc8449e1bfdd8c60577bddaaeb62b01f7e7c
raja.lmsace <raja.lmsace@gmail.com>
2026-03-10 21:00:16 +0530

    Dash Multi category field support added - DASH-1149, DASH-1152.

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025103003;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026031000;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700;        // Requires this Moodle version.
 $plugin->component = 'block_dash';      // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = '2.5';
+$plugin->release = '2.8';
 $plugin->supported = [401, 500];

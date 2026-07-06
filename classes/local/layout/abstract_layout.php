36515775f464985d5772719f13219422e222665c
abinesh <abinesh.s@lmsace.com>
2026-07-06 21:16:08 +0530

    Bugfix: Hide details area fields for widgets that don't support the details area tab - DASH-1251

--- classes/local/layout/abstract_layout.php
+++ classes/local/layout/abstract_layout.php
@@ -68,12 +68,21 @@ abstract class abstract_layout implements layout_interface, \templatable {
         $this->datasource = $datasource;
         // Inject the details area table so every data source offers details button/link fields.
         // Pass the block instance ID so detail IDs are unique when multiple blocks are on one page.
-        $blockinstanceid = 0;
-        $bi = $datasource->get_block_instance();
-        if ($bi && isset($bi->instance->id)) {
-            $blockinstanceid = (int) $bi->instance->id;
+        $supportsdetailstab = true;
+        if (method_exists($datasource, 'get_preferences_form_tabs')) {
+            $supportsdetailstab = in_array(
+                \block_dash\local\data_source\form\preferences_form::TAB_DETAILS,
+                $datasource->get_preferences_form_tabs()
+            );
         }
-        $this->datasource->add_table(new details_area_table($blockinstanceid));
+        if ($supportsdetailstab) {
+            $blockinstanceid = 0;
+            $bi = $datasource->get_block_instance();
+            if ($bi && isset($bi->instance->id)) {
+                $blockinstanceid = (int) $bi->instance->id;
+            }
+            $this->datasource->add_table(new details_area_table($blockinstanceid));
+        }
     }
 
     /**
